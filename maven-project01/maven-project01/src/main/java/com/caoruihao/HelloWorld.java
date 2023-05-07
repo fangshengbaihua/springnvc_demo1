e5181a5b76255e9071a3540ca52c75b6f99ea4b9
fangshengbaihua <303222755@qq.com>
2023-05-07 16:15:22 +0800

    完成第print12223功能

--- maven-project01/maven-project01/src/main/java/com/caoruihao/HelloWorld.java
+++ maven-project01/maven-project01/src/main/java/com/caoruihao/HelloWorld.java
@@ -3,7 +3,7 @@ package com.caoruihao;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("HelloWorld1223");
+        System.out.println("HelloWorld12223");
 
         System.out.println("HelloWorld");
 
